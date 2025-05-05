4ef451be9f215724e04b12f8f86f96c2e271982d
Jamie Braaksma <codingdad@outlook.com>
2025-05-05 13:18:20 -0600

    Update to use modern Microsoft.Data.SqlClient

--- Loggers/DatabaseLoggers/SqlServerLogger.cs
+++ Loggers/DatabaseLoggers/SqlServerLogger.cs
@@ -1,50 +1,50 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Data.SqlClient;
 using System;
-using System.Data.SqlClient;
 
 namespace CodingDad.NET.Common.Loggers.DatabaseLoggers
 {
-	public class SqlServerLogger : DbLoggerBase
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SqlServerLogger"/> class.
-		/// </summary>
-		/// <param name="connectionString">The SQL Server database connection string.</param>
-		/// <param name="logTable">The table name for storing logs.</param>
-		/// <param name="minLogLevel">The minimum log level to log.</param>
-		public SqlServerLogger (string connectionString, string logTable, LogLevel minLogLevel)
-			: base(connectionString, logTable, minLogLevel)
-		{
-		}
+    /// <summary>
+    /// Provides a logging service using Microsoft.Data.SqlClient to write logs into a SQL Server table.
+    /// </summary>
+    public class SqlServerLogger : DbLoggerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlServerLogger"/> class.
+        /// </summary>
+        /// <param name="connectionString">The SQL Server database connection string.</param>
+        /// <param name="logTable">The table name for storing logs.</param>
+        /// <param name="minLogLevel">The minimum log level to log.</param>
+        public SqlServerLogger (string connectionString, string logTable, LogLevel minLogLevel)
+            : base(connectionString, logTable, minLogLevel)
+        {
+        }
 
-		/// <summary>
-		/// Logs the message to the SQL Server database.
-		/// </summary>
-		protected override void LogToDatabase (LogLevel logLevel, string message, Exception exception)
-		{
-			try
-			{
-				using (SqlConnection connection = new SqlConnection(ConnectionString))
-				{
-					string query = $"INSERT INTO {LogTable} (Timestamp, LogLevel, Message) VALUES (@Timestamp, @LogLevel, @Message)";
+        /// <summary>
+        /// Logs the message to the SQL Server database using Microsoft.Data.SqlClient.
+        /// </summary>
+        /// <param name="logLevel">The log level of the message.</param>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional exception associated with the log entry.</param>
+        protected override void LogToDatabase (LogLevel logLevel, string message, Exception exception)
+        {
+            try
+            {
+                using var connection = new SqlConnection(ConnectionString);
+                var query = $"INSERT INTO {LogTable} (Timestamp, LogLevel, Message) VALUES (@Timestamp, @LogLevel, @Message)";
 
-					using (SqlCommand cmd = new SqlCommand(query, connection))
-					{
-						cmd.Parameters.AddWithValue("@Timestamp", DateTime.UtcNow);
-						cmd.Parameters.AddWithValue("@LogLevel", logLevel.ToString());
-						cmd.Parameters.AddWithValue("@Message", message);
+                using var cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Timestamp", DateTime.UtcNow);
+                cmd.Parameters.AddWithValue("@LogLevel", logLevel.ToString());
+                cmd.Parameters.AddWithValue("@Message", message);
 
-						connection.Open();
-						cmd.ExecuteNonQuery();
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				// Handle logging failure (e.g., log to a fallback location)
-				// Fallback logging logic can go here
-				throw new Exception("An error occurred while logging to the SQL Server database.", ex);
-			}
-		}
-	}
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while logging to the SQL Server database.", ex);
+            }
+        }
+    }
 }
